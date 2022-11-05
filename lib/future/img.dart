b7da8069d0e6293413eee869dd2f25e88924b45b
leedayun <kiwidy@naver.com>
2022-11-06 04:32:07 +0900

    토픽, 포스트 수정

--- lib/future/img.dart
+++ lib/future/img.dart
@@ -3,17 +3,19 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
 
-Future<dynamic>upload(String userId, String content, int topicId) async{
-  Map<String, String> headers={
-    'Content-Type':'multipart/form-data'
-  };
+Future<dynamic>upload(dynamic input) async{
+  var dio = new Dio();
+  try{
+    dio.options.contentType='multipart/form-data';
+    dio.options.maxRedirects.isFinite;
 
-  final response=await http.post(Uri.http('http://13.125.205.227:3000/feed/upload'));
-  if(response.statusCode == 200) {
-    Map<String, dynamic> json=jsonDecode(response.body);
-    return Img(url: json['img']);
+    var response=await dio.patch('http://13.125.205.227:3000/feed/upload', data: input);
+
+    return response.data;
   }
-  else throw Exception('Failed to load User');
+  catch(e){
+    print(e);
+  }
 }
 
 class Img{
--- lib/future/post.dart
+++ lib/future/post.dart
@@ -6,13 +6,13 @@ Future<dynamic>addPost(String userId, String content, int topicId) async{
   final response=await http.post(Uri.http('http://13.125.205.227:3000/feed/post'));
   if(response.statusCode == 200) {
     Map<String, dynamic> json=jsonDecode(response.body);
-    return Post(userId: userId, content: content, topicId: topicId, postId: json['post_id']);
+    return json['post_id'];
   }
   else throw Exception('Failed to load User');
 }
 
 Future<List>getRandomFeed(int topicId) async{
-  final response=await http.post(Uri.parse('http://13.125.205.227:3000/feed/hot'),  body:<String, String>{'user_id': userId, 'untopic_id':topicId});
+  final response=await http.post(Uri.parse('http://13.125.205.227:3000/feed/hot'),  body:<String, dynamic>{'topic_id':topicId});
   if(response.statusCode == 200) {
     List list=jsonDecode(response.body);
     return list;
@@ -21,7 +21,7 @@ Future<List>getRandomFeed(int topicId) async{
 }
 
 Future<bool>postLike(String userId, String postId, bool isLike) async{
-  final response=await http.post(Uri.parse('http://13.125.205.227:3000/feed/like'),  body:<String, String>{'user_id': userId, 'post_id': postId, 'is_like': isLike});
+  final response=await http.post(Uri.parse('http://13.125.205.227:3000/feed/like'),  body:<String, dynamic>{'user_id': userId, 'post_id': postId, 'is_like': isLike});
   if(response.statusCode == 200) {
     bool result=jsonDecode(response.body);
     return result;
@@ -29,15 +29,4 @@ Future<bool>postLike(String userId, String postId, bool isLike) async{
   else throw Exception('Failed to load User');
 }
 
-class Post{
-  final String userId;
-  final String content;
-  final int postId;
-  final int topicId;
-  final String? img;
 
-  Post({required this.userId, required this.content, required this.postId, required this.topicId});
-  factory Post.fromJson(Map<String, dynamic> json){
-    return Post(userId: json[], content: json[], postId: json[], topicId: json[]);
-  }
-}
