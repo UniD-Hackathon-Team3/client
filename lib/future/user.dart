3ecdf6b01491a590b56a912d41a852419a17ed3f
leedayun <kiwidy@naver.com>
2022-11-05 21:51:29 +0900

    post 메소드로 변환

--- lib/future/user.dart
+++ lib/future/user.dart
@@ -1,32 +1,69 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/material.dart';
+
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
-Future<User>fetchUser() async{
-  final response=await http.get(Uri.http(''));
+Future<dynamic>signupUser(String userId, String userPw) async{
+  Map<String, String> headers={
+    'Content-Type':'application/json'
+  };
+
+  final response=await http.post(Uri.http(''), headers: headers, body:<String, String>{'user_id': userId, 'user_pw': userPw});
+  if(response.statusCode == 200) return User(userId: userId, userPw: userPw);
+  else {
+    throw Exception('Failed to signup User');
+  }
+}
+Future<dynamic>loginUser(String userId, String userPw) async{
+  Map<String, String> headers={
+    'Content-Type':'application/json'
+  };
+
+  final response=await http.post(Uri.http(''), headers: headers, body:<String, String>{'user_id': userId, 'user_pw': userPw});
+  if(response.statusCode == 200) return User(userId: userId, userPw: userPw);
+  else {
+    throw Exception('Failed to login User');
+  }
+}
+
+Future<dynamic>getuserInfo(String userId) async{
+  Map<String, String> headers={
+    'Content-Type':'application/json'
+  };
+
+  final response=await http.post(Uri.http(''), headers: headers, body:<String, String>{'user_id': userId});
+  if(response.statusCode == 200) {
+    Map<String, dynamic> json=jsonDecode(response.body);
+    return User(userId: userId, posts: json['posts'], followers: json['followers']);
+  }
+  else {
+    throw Exception('Failed to load UserInfo');
+  }
+}
+
+Future<dynamic>voteTopic(String userId, String topicId) async{
+  Map<String, String> headers={
+    'Content-Type':'application/json'
+  };
+
+  final response=await http.post(Uri.http(''), headers: headers, body:<String, String>{'user_id': userId, 'topic_id':topicId});
   if(response.statusCode == 200) return User.fromJson(jsonDecode(response.body));
   else throw Exception('Failed to load User');
-}
-Future<Topic>fetchTopic() async{
-  final response=await http.get(Uri.http(''));
-  if(response.statusCode == 200) return Topic.fromJson(jsonDecode(response.body));
-  else throw Exception('Failed to load Topic');
-}
-Future<Post>fetchPost() async{
-  final response=await http.get(Uri.http(''));
-  if(response.statusCode == 200) return Post.fromJson(jsonDecode(response.body));
-  else throw Exception('Failed to load Post');
 }
 
 class User{
   final String userId;
-  final String userPw;
+  final String? userPw;
+  List? posts;
+  int? followers;
 
-  User({required this.userId, required this.userPw});
+  User({required this.userId, this.userPw, this.posts, this.followers});
   factory User.fromJson(Map<String, dynamic> json){
     return User(userId: json['result'], userPw: json['result']);
   }
+
+
 }
 class Topic{
   final String title;
@@ -36,6 +73,7 @@ class Topic{
   factory Topic.fromJson(Map<String, dynamic> json){
     return Topic(title: json[], userId: json[]);
   }
+
 }
 class Post{
   final String userId;
