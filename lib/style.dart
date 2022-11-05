5afc12183f2dc075e0d2c75fabaeee1d3799bc09
kor-ash <tjdgh4540@korea.ac.kr>
2022-11-05 19:40:39 +0900

    search

---
+++ lib/style.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+var textstyle1 = TextStyle(
+  fontSize: 15.0,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+var textstyle2 = TextStyle(
+  fontSize: 18.0,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+
+var theme = ThemeData(  //appbar의 테마 설정
+  appBarTheme: AppBarTheme(
+    color: Color(0xFF716EFF),
+    elevation: 0,
+    titleTextStyle: TextStyle(color: Colors.white, fontSize: 50),
+    actionsIconTheme: IconThemeData(color: Colors.black),
+
+  ),
+  iconTheme: IconThemeData(
+    color: Colors.black,
+  ),
+);
